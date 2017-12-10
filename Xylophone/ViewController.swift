28d35b6a5520cfd20c4308addc26d683e25b002d
Michail Mishev <m.mishev1@gmail.com>
2017-12-11 00:38:34 +0200

    Design and Outlets

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Michail Mishev on 11/12/17.
+//  Copyright © 2017 Michail Mishev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    
+
+}
+
